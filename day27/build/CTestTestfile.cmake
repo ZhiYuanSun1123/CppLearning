1ec8901c97885736ab50df615183a37915582bda
BoilFish <boilfish23@gmail.com>
2026-08-24 23:49:45 +0800

    day29

---
+++ day27/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/zhubulandeshuizhuyu/Project/Code/C++/CppLearning/day27
+# Build directory: /Users/zhubulandeshuizhuyu/Project/Code/C++/CppLearning/day27/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test("tensor_metadata_tests" "/Users/zhubulandeshuizhuyu/Project/Code/C++/CppLearning/day27/build/tensor_metadata_tests")
+set_tests_properties("tensor_metadata_tests" PROPERTIES  _BACKTRACE_TRIPLES "/Users/zhubulandeshuizhuyu/Project/Code/C++/CppLearning/day27/CMakeLists.txt;87;add_test;/Users/zhubulandeshuizhuyu/Project/Code/C++/CppLearning/day27/CMakeLists.txt;0;")
